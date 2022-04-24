234c10abc5fdb67a3059fdf239fd12153d072209
saarika-arch <Saarika.S@cognizant.com>
2022-04-24 17:35:45 +0530

    add changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Markdown Viewer`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@saarika`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,44 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import './index.css'
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new ArchNeo site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+export default class IndexPage extends React.Component {
+  constructor(inProps) {
+    super(inProps);
+    console.log(inProps);
+    this.data=inProps.data;
+  }
+  render() {
+    return (
+      //JSX
+      <Layout>
+        <Seo title="Home" />
+        <div class="outerContainer">
+          <h2>{this.data.site.siteMetadata.description}</h2>
+          <h3>{this.data.site.siteMetadata.author}</h3>
+          <StaticImage
+            src="../images/gatsby-astronaut.png"
+            width={300}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt="A Gatsby astronaut"
+            style={{ marginBottom: `1.45rem` }}
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
 
-export default IndexPage
+export const pageQuery = graphql`
+query{
+  site{
+    siteMetadata{
+      title
+      description
+      author
+    }
+  }
+}`
